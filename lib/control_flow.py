026ce5dee008ce7e5f8b426ac6588059ab428622
samakpan-sp <samakpan817@gmail.com>
2023-10-02 12:35:04 +0100

    Assignment completed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,65 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
+   if username == 'admin' and password == '12345':
+    return 'Access granted'
+   elif username == 'ADMIN' and password == '12345':
+      return 'Access granted'
+   else:
+    return 'Access denied'
     # your code here
-    pass
+ 
 
 def hows_the_weather(temperature):
+    if temperature < 40:
+       return "It's brisk out there!"
+    elif temperature <= 65:
+       return "It's a little chilly out there!"
+    elif temperature > 85:
+       return "It's too dang hot out there!"
+    else:
+       return "It's perfect out there!"
     # your code here
-    pass
+    # pass
 
 def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+       return "FizzBuzz"
+    elif num % 5 == 0:
+       return "Buzz"
+    elif num % 3 == 0:
+       return "Fizz"
+    else:
+       return num
     # your code here
-    pass
+#     pass
+# print(fizzbuzz(15))
+
+# def calculator(operation, num1, num2):
+#     # your code here
+#     if operation == "+":
+#       return num1 + num2
+#     elif operation == "-":
+#        return num1 - num2
+#     elif operation == "*":
+#        return num1 * num2
+#     elif operation == "/":
+#        return num1 / num2
+#     else:
+#        raise ValueError("Invalid operation!")
+#     pass
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
+
+
